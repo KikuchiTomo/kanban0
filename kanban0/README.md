06b720431de86f814abd2904f842846f23e17beb
KikuchiTomo <tomo_kikuchi@icloud.com>
2019-11-03 00:02:31 +0900

    fixed

--- kanban0/README.md
+++ kanban0/README.md
@@ -4,7 +4,7 @@ webアプリ用看板システム
 
 練習で作ってるので適当です。遊び程度でお願いします
 
-Ajaxとformに対応指定ます
+~~Ajaxとformに対応指定ます~~必要性がないのでAjaxのみの対応にします
 
 まだバグだらけなのでコミットしません
 
@@ -15,10 +15,10 @@ Ajaxとformに対応指定ます
 <link rel="stylesheet" href="../lib/css/kanban0.css">
 <script src="../lib/kanban0.js"></script>
 ```
-で読み込みます
+と読み込みます
 使用したい場所に
 ```html
-<div class="kanban-area">
+<div class="kanban-area" data-kanbanarea="0">>
     <!--使用したい個数分かく -->
     <div class="kanban" data-kanbanid="0" data-disabled="0"     data-title="kanban0"></div>
     <div class="kanban" data-kanbanid="1" data-disabled="0,1,2" data-title="kanban1"></div>
@@ -29,7 +29,11 @@ Ajaxとformに対応指定ます
     
 と記述します
 
-data-disabledには特定の看板からのドロップ禁止が指定できます。指定はkanbanidで使用します
+data-disabledには特定の看板からのドロップをkanbanidで禁止が指定できます。
+
+怠けなのですがdata-kanbanidは*必ず重複させない*でください。Node順にidを割り振ってください。
+初期段階でdata-kanbanidに依存する設計にしてしまったので直すのだるいです。
+issueたててそのうち直します
 
 また、data-kanbanに看板のidを割振れます。APIから看板を増やす場合は場合は一番おおきいid+1のidが割り振られます
 
@@ -44,9 +48,6 @@ htmlでのidは各看板に`id="kanban-%d-%d"`で割り振られます
     var kanban_0 = new kanban0({
         ajax_mode    : false,                 //Ajaxで非同期通信する場合はtrue
         ajax_host    : "https://example.com"  //Ajaxでのpost先
-        form_mode    : false,                 //formを使うか
-        form_action  : "/end_point"           //formのaction指定
-        form_method  : "post"                 //formのmethot指定
     });
 </script>
 ```
@@ -60,19 +61,18 @@ htmlでのidは各看板に`id="kanban-%d-%d"`で割り振られます
 以下にAPIを載せておきます
 
 
-| Method         | Arguments                            | desc                                                                                    |
-|----------------|--------------------------------------|-----------------------------------------------------------------------------------------|
-| setEventItem   | itemid<br>eventName,<br> eventHander | アイテムにイベントハンドラを追加します<br>drop,dragS,dragE,clickが指定できます          |
-| addKanban      | title                                | Kanbanを生成します。タイトルとその他の色を指定します。                                  |
-| addItem        | kanbanid,<br>content, <br>font-color | itemを追加します。追加先のkanbanidと内容を指定します                                    |
-| getKanbanId    | itemid                               | itemidに対する親要素のIDを取得します                                                    |
-| getItemId      | kanbanid                             | kanbanidに対する子要素のIDを取得します。配列を返します                                  |
-| getAllItemId   |                                      | 全てのitemのIDを取得します                                                              |
-| getAllKanbanId |                                      | 全てのkanbanのIDを取得します                                                            |
-| removeKanban   | kanbanid                             | 指定したIDのkanbanを削除します                                                          |
-| removeItem     | itemid                               | 指定したIDのitemを削除します                                                            |
-| sendHttpReq    |                                      | 呼び出したタイミングでAjaxでpostします<br>コンストラクタでfalseにしていると動作しません |
-| formSubmit     |                                      | 呼び出したタイミングでFormをpostします<br>コンストラクタでfalseにしていると動作しません |
+| Method         | Arguments                          | desc                                                                                    |
+|----------------|------------------------------------|-----------------------------------------------------------------------------------------|
+| setEventItem   | itemId<br>eventName<br>eventHander | アイテムにイベントハンドラを追加します<br>drop,dragS,dragE,clickが指定できます          |
+| addKanban      | title<br>kanbanAreaId              | Kanbanを生成します。タイトルとその他の色を指定します。                                  |
+| addItem        | kanbanId<br>content<br>disabled_id | itemを追加します。追加先のkanbanidと内容を指定します                                    |
+| getKanbanId    | itemId                             | itemidに対する親要素のIDを取得します                                                    |
+| getItemId      | kanbanId                           | kanbanidに対する子要素のIDを取得します。配列を返します                                  |
+| getAllItemId   |                                    | 全てのitemのIDを取得します                                                              |
+| getAllKanbanId |                                    | 全てのkanbanのIDを取得します                                                            |
+| removeKanban   | kanbanId<br>kanbanAreaId           | 指定したIDのkanbanを削除します                                                          |
+| removeItem     | itemId                             | 指定したIDのitemを削除します                                                            |
+| sendHttpReq    |                                    | 呼び出したタイミングでAjaxでpostします<br>コンストラクタでfalseにしていると動作しません |
 
 
 
--- kanban0/lib/kanban0.js
+++ kanban0/lib/kanban0.js
@@ -1,50 +1,110 @@
-export default class kanban0 {
+class kanban0 {
     constructor(config) {
         //user-config
         this.thema_type_  = config.thema_type  || 0;
         this.ajax_mode_   = config.ajax_mode   || false;
         this.ajax_host_   = config.ajax_host   || 'http://127.0.0.1';
-        this.form_mode_   = config.form_mode   || false;
-        this.form_action_ = config.form_action || '/';
-        this.form_methot_ = config.form_methot || 'get';
 
         //create kanbans
-        this.kanbans_     = document.getElementByClassName('kanban');
+        this.kanbans_     = document.getElementsByClassName('kanban');
         this.insert_      = []
 
         let kanbans_len      = this.kanbans_.length;
         
         //kanbanにid割り当て
         for(let j=0; j<kanbans_len; j++){
-            let kanban_elem        = kanbans[j]
+            let kanban_elem        = this.kanbans_[j]
             const kanban_id        = kanban_elem.dataset.kanbanid;
             const kanban_id00      = ('00' + kanban_id).slice(-2);  //zero_padding
             kanban_elem.id         = `kanban-${kanban_id00}`;
         }
 
-        this.kanbans_     = document.getElementByClassName('kanban');
+        this.kanbans_     = document.getElementsByClassName('kanban');
         //具体的なのを作る
         for(let i=0; i<kanbans_len; i++){
             let header_div       = document.createElement('div');
-            header_div.innerHTML = kanbans_[i].dataset.title;
-            header_div.id        = `${kanbans_[i].id}-title`
+            let main_div         = document.createElement('div');
+            header_div.innerHTML = this.kanbans_[i].dataset.title;
+            main_div.id          = `${this.kanbans_[i].id}-main`
+            header_div.id        = `${this.kanbans_[i].id}-title`
+            main_div.className   = 'kanban-droparea'
             header_div.className = 'kanban-title'
             this.kanbans_[i].appendChild(header_div);
+            this.kanbans_[i].appendChild(main_div);
         }
         
         for(let i=0; i<kanbans_len; i++){
             this.kanbans_[i].addEventListener('drop', function(event){
                 event.dataTransfar.dropEffect = 'move';
             }, false);
-
-            this.kanbans_[i].addEventListener('dragover', function(event){
-                
-            }, false);
         }
         
     }
 
-    this.setKanbanBakCol(kanban_id, color, constant){
+    addKanban(title0, parent_id0, disabled_id0){
+        var title       = title0       || 'kanban';
+        var disabled_id = disabled_id0 || '';
+
+        if(parent_id0==undefined || parent_id0==null){
+            console.log("Error(kanban0.addKanban): parent_id is null or undefined. parent_id is needed. ");
+            return -1;
+        } 
         
+        var parents = document.getElementsByClassName('kanban-area');
+        this.kanbans_ = document.getElementsByClassName('kanban');
+        for(let i=0; i<parents.length; i++){
+
+            //全ての看板を取得して一番おおきいid+1する
+            var kanban_ids=[];
+            for(let j=0; j<this.kanbans_.length; j++) kanban_ids.push(Number(this.kanbans_[j].dataset.kanbanid));
+
+            //kanbanがないとき
+            const new_id   = Math.max.apply(null,kanban_ids);
+            if(new_id==-Infinity) kanban_ids.push(-1);
+            
+            if(parents[i].dataset.kanbanarea==parent_id0){
+                let new_kanban = document.createElement('div');
+                const new_id   = Math.max.apply(null,kanban_ids) + 1;
+                const new_id00 = ('00' + new_id).slice(-2);
+                new_kanban.dataset.kanbanid = new_id
+                new_kanban.dataset.disabled = disabled_id;
+                new_kanban.dataset.title    = title0;
+                new_kanban.className        = 'kanban';
+                new_kanban.id               = `kanban-${new_id00}`
+
+                let header_div       = document.createElement('div');
+                let main_div         = document.createElement('div');
+                header_div.innerHTML = title;
+                main_div.id          = `kanban-${new_id00}-main`
+                header_div.id        = `kanban-${new_id00}-title`
+                main_div.className   = 'kanban-droparea'
+                header_div.className = 'kanban-title'
+
+                new_kanban.appendChild(header_div);
+                new_kanban.appendChild(main_div);
+
+                parents[i].appendChild(new_kanban);
+            }
+        }
+    }
+
+    removeKanban(elem_id, area_id){
+        if(elem_id==undefined || elem_id==null){
+            console.log("Error(kanban0.removeKanban): kanban_id is null or undefined. kanban_id is needed. ");
+            return -1;
+        } 
+        
+        var parents = document.getElementsByClassName('kanban-area');
+        for(let i=0; i<parents.length; i++){
+            if(parents[i].dataset.kanbanarea==area_id){
+                const id00 = ('00' + elem_id).slice(-2);
+                let remove_elem = document.getElementById(`kanban-${id00}`)
+                if(remove_elem==undefined){
+                    console.log(`Error(kanban0.removeKanban): kanban-${id00} is undefined.`);
+                    return -1;
+                }
+                remove_elem.parentNode.removeChild(remove_elem);
+            }
+        }
     }
 }
